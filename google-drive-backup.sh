129554cfb2f1f676675b1ba2312e4c4006186ba1
ameeking <aaronmeeking@gmail.com>
2020-04-20 14:32:22 +1000

    Back up photos to google drive using rclone

---
+++ google-drive-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if pidof -o %PPID -x "google-drive-backup.sh"; then
+    echo "google-drive-backup already running"
+    exit 1
+fi
+
+# logfile
+LOG_FILE="/mnt/media1/logs/google-drive-backup.log"
+
+# backup location from
+FROM_PATH="/mnt/media1/photos/"
+
+# backup location to
+TO_PATH="google-drive:/Photos/"
+
+# bandwidth limit schedule
+BW_LIMIT="07:00,5M 23:00,off"
+
+echo "rclone sync started $(date)" >> $LOG_FILE 2>&1
+
+rclone sync $FROM_PATH $TO_PATH --bwlimit "$BW_LIMIT" --log-file $LOG_FILE -v
+
+if [ $? -eq 0 ]; then
+    echo "rclone sync completed $(date)" >> $LOG_FILE 2>&1
+    exit 0
+else
+    echo "rclone sync failed $(date). rclone exit code $?" >> $LOG_FILE 2>&1
+    exit 1
+fi
