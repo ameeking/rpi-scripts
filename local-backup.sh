b98c3cce3423ae6a04c61d54c3ea9d267d5de369
ameeking <aaronmeeking@gmail.com>
2020-04-21 11:15:09 +1000

    Updated logging

--- local-backup.sh
+++ local-backup.sh
@@ -14,14 +14,14 @@ FROM_PATH="/mnt/media1/photos/"
 # backup location to
 TO_PATH="/mnt/backup1/photos"
 
-echo "rsync started $(date)" >> $LOG_FILE 2>&1
+echo "local backup started $(date)" >> $LOG_FILE 2>&1
 
 rsync -avu --delete $FROM_PATH $TO_PATH --log-file=$LOG_FILE
 
 if [ $? -eq 0 ]; then
-    echo "rsync completed $(date)" >> $LOG_FILE 2>&1
+    echo "local backup completed $(date)" >> $LOG_FILE 2>&1
     exit 0
 else
-    echo "rsync failed $(date). rclone exit code $?" >> $LOG_FILE 2>&1
+    echo "local backup failed $(date). rclone exit code $?" >> $LOG_FILE 2>&1
     exit 1
 fi
